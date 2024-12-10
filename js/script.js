77ed2f6f76261cb45768fe5c29c29ed099902496
WESLLEY HOLANDA SANTOS <weslley.hsantos2@senacsp.edu.br>
2024-12-10 11:48:51 -0300

    atualizações

--- js/script.js
+++ js/script.js
@@ -8,15 +8,11 @@ $('.slide-banner').slick({
 });
 
 
-$(document).ready(function() {
-    $('.marcas-logo').slick({
-        infinite: true,
-        slidesToShow: 4, 
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false, 
-        dots: false,   
-    });
+$('.marcas-logo').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: false,
+    autoplaySpeed: 2000,
 });
 
